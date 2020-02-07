1bfc69d5a809dce6c24ae363f706bf9a2ef61db0
franciscojgsalguero <44877788+franciscojgsalguero@users.noreply.github.com>
2020-02-07 23:51:34 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,6 +1,45 @@
 const app = new Vue({
-	el: 'app',
+	el: '#app',
 	data: {
-		titulo: 'Shopping List'
-	}
+		titulo: 'Shopping List',
+		productos: [
+		],
+		nombre: '',
+		preu: 0,
+		cantidad: 0,
+		total: 0,
+		total_producto: 0
+	},
+	methods: {
+		agregarProducto () {
+			this.productos.push({
+				nombre: this.nombre , preu: this.preu, cantidad: 0, total_producto: this.preu * this.cantidad,
+			});
+			this.nombre = '';
+			this.preu = 0;
+		}
+	},
+	computed: {
+		sumarproducto () {
+			this.total = 0;
+			for(producto of this.productos) {
+				producto.total_producto = producto.preu * producto.cantidad;
+			}
+			this.sumarProductos;
+			this.total = 0;
+
+			return producto.total_producto;
+		},
+		sumarProductos () {
+				this.total = 0;
+				for(producto of this.productos) {
+					this.total = this.total + producto.preu;
+				}
+
+				return this.total;
+		},
+		eliminar () {
+			this.productos.pop();
+		}
+	},
 })
